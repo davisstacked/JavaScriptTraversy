83f9a9370c79802526ddb15a803dea6c44a1ba52
davisstacked <brettzvi@gmail.com>
2021-04-14 08:43:16 -0700

    Section 75

---
+++ Section 73 - API PROJECTS/github.js
@@ -0,0 +1,16 @@
+class Github {
+  constructor() {
+    this.client_id = '12f12736467b9e08b653';
+    this.client_secret = '2eaaa5c6193bc1a6049c388e3fc088ab7a220a3f'
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+
+    return {
+      profile
+    }
+  }
+}
