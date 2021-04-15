8dbb0383b5ea56735dce9d6270df421658c492b8
davisstacked <brettzvi@gmail.com>
2021-04-14 18:19:37 -0700

    section 78

--- Section 73 - API PROJECTS/github.js
+++ Section 73 - API PROJECTS/github.js
@@ -1,16 +1,22 @@
 class Github {
   constructor() {
     this.client_id = '12f12736467b9e08b653';
-    this.client_secret = '2eaaa5c6193bc1a6049c388e3fc088ab7a220a3f'
+    this.client_secret = '2eaaa5c6193bc1a6049c388e3fc088ab7a220a3f';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
