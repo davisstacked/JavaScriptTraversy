8970c083d27a0625eb2441f5e7980bad6c17d802
davisstacked <brettzvi@gmail.com>
2021-04-21 14:11:03 -0700

    section 98

---
+++ Section 98 - Observer Pattern/app.js
@@ -0,0 +1,60 @@
+// Can be used for subscriptions
+
+class EventObserver {
+  constructor() {
+    this.observers = [];
+  }
+  subscribe(fn) {
+    this.observers.push(fn);
+    console.log(`You are now subscribed to ${fn.name}`);
+  }
+  
+  unsubscribe(fn) {
+    // filter out from the list whatever matches the callback function. if there is no match, the callback gets to stay on the list. the filter returns a new list and reassigns the list of observers.
+    this.observers = this.observers.filter(item => {
+      if (item !== fn) {
+        return item;
+      }
+    });
+    console.log(`You are now unsubscribed from ${fn.name}`)
+  }
+  
+  fire() {
+    this.observers.forEach(item => {
+      item.call();
+    });
+  }
+}
+
+
+const click = new EventObserver();
+
+// Event Listeners
+document.querySelector('.sub-ms').addEventListener('click', function () {
+  click.subscribe(getCurMilliseconds);
+});
+
+document.querySelector('.unsub-ms').addEventListener('click', function () {
+  click.unsubscribe(getCurMilliseconds);
+});
+
+document.querySelector('.fire').addEventListener('click', function () {
+  click.fire();
+});
+
+document.querySelector('.sub-s').addEventListener('click', function () {
+  click.unsubscribe(getCurMilliseconds);
+});
+
+document.querySelector('.unsub-s').addEventListener('click', function () {
+  click.fire();
+});
+
+// Click handler
+const getCurMilliseconds = function () {
+  console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+}
+
+const getCurSeconds = function () {
+
+}
